7ce331f2b2eb51077b9ac66f9b197dd5d67ed6f9
jarmanso7 <jordi.armangue@hotmail.com>
2019-01-31 16:09:49 +0100

    Create hide-furigana.js

---
+++ hide-furigana.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         Hide Furigana in Jisho Sentences
+// @version      0.1
+// @description  Enables a link to toggle between Furigana On/Off in the page of sentences at Jisho.org
+// @author       jarmanso7
+// @match        https://jisho.org/search/*#sentences
+// @match        https://jisho.org/search/*%20%23sentences
+// ==/UserScript==
+
+function AddToggleFuriganaLink(){
+	/* ------------------------ GENERATE CUSTOM LINK ---------------------*/
+	var addToggleFuriganaLink = document.createElement ('a');
+	addToggleFuriganaLink.setAttribute ('id', 'toggle-furigana-custom-link');
+	addToggleFuriganaLink.setAttribute ('class', 'concept_light-status_link');
+	addToggleFuriganaLink.innerText = 'Furigana off';
+
+	var sentences = document.getElementsByClassName("sentences")[0]
+
+	var hrDivider = document.createElement ( 'hr' );
+
+	sentences.insertBefore(hrDivider, sentences.firstChild);
+	sentences.insertBefore(addToggleFuriganaLink, sentences.firstChild);
+
+	/* ------------------------ ACTIVATE CUSTOM LINK ---------------------*/
+	document.getElementById ('toggle-furigana-custom-link').addEventListener(
+		'click', ButtonClickAction, false
+	);
+
+	function ButtonClickAction (zEvent) {
+		ToggleFuriganaEvent();
+	}
+}
+
+function ToggleFuriganaEvent(){
+
+	debugger;
+
+	var furiganaLink = document.getElementById ('toggle-furigana-custom-link');
+
+	if (furiganaLink.innerText == 'Furigana off'){
+		LoopThroughFuriganaTagsChangeDisplayProperty('none');
+		furiganaLink.innerText = 'Furigana on';
+	}
+	else {
+		LoopThroughFuriganaTagsChangeDisplayProperty('block');
+		furiganaLink.innerText = 'Furigana off';
+	}
+}
+
+function LoopThroughFuriganaTagsChangeDisplayProperty(displayValue){
+
+	var furiganaTags = document.getElementsByClassName("furigana");
+
+	for (var i = 0; i < furiganaTags.length; i++) {
+	  furiganaTags[i].style.display = displayValue;
+	}
+}
+
+//Main function
+(function() {
+	AddToggleFuriganaLink();
+})();
